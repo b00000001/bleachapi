4348328332e1b14c3c8ecc05f62651596004d2d0
Aaron Deas <deas.aaron@gmail.com>
2021-11-16 18:47:16 -0500

    Changed jquery so that page will omit descriptions that are unable to be captured (ie. Yoruichi)

--- src/scraper/index.ts
+++ src/scraper/index.ts
@@ -3,9 +3,9 @@ import fetchPage from './axios';
 import axios from 'axios';
 import { text } from 'express';
 const charname = {
-  first: 'Byakuya',
-  last: 'Kuchiki'
-}
+  first: 'Yoruichi',
+  last: `Shihoin`
+};
 const pageInfo = async () => {
   try {
     const { data }: any = await axios.get(
@@ -18,8 +18,23 @@ const pageInfo = async () => {
         .text()
         .replace(/\s\s+/g, ''),
       image: $('#mw-content-text img').attr('src'),
-      description: $('#mw-content-text p:contains("Byakuya")').first().text().replace(/\s\s+/g, ''),
-      powersandabilities: $('#mw-content-text p:nth-child(15)').text(),      
+      description: `${
+        charname.first === 'Yoruichi'
+          ? 'Not Available'
+          : $(
+              `#mw-content-text b:contains("${
+                charname.first || charname.last
+              }") ~ i ~ a`
+            )
+              // .first()
+              .text()
+              .replace(/\s\s+/g, '')
+      }`,
+      powersandabilities: $(
+        '#mw-content-text h3:contains("Powers & Abilities") ~ p, #mw-content-text h3:contains("Natural Abilities") ~ p, #mw-content-text h2:contains("Powers & Abilities") ~ p'
+      )
+        .text()
+        .split(/\r?\n/)
     };
     return pageInfo;
   } catch (err) {
